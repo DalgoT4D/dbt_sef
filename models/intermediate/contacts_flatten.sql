fa67cf40803ee8e0219d27a9d30006662e5a368c
Ishankoradia <ikoradia@umich.edu>
2025-11-27 14:50:04 +0530

    contacts flatten

---
+++ models/intermediate/contacts_flatten.sql
@@ -0,0 +1,17 @@
+select
+    id,
+    uuid,
+    is_deleted,
+    urn,
+    JSON_EXTRACT_SCALAR(details, '$.name') AS full_name,
+    JSON_EXTRACT_SCALAR(details, '$.language_select') AS full_name,
+    JSON_EXTRACT_SCALAR(details, '$.designation') AS designation,
+    JSON_EXTRACT_SCALAR(details, '$.state') AS state,
+    JSON_EXTRACT_SCALAR(details, '$.grade') AS grade,
+    JSON_EXTRACT_SCALAR(details, '$.grades') AS grades,
+    JSON_EXTRACT_SCALAR(details, '$.whatsapp_id') AS whatsapp_id,
+    JSON_EXTRACT_SCALAR(details, '$.whatsapp_profile_name') AS whatsapp_profile_name,
+    inserted_at,
+    updated_at
+from {{ source('sef_whatsapp_bot', 'contacts') }}
+where is_deleted is false
