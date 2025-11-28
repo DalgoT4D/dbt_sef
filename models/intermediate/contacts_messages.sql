b043812d43194432e142a7a9a7cb150cf5b7a694
Ishankoradia <ikoradia@umich.edu>
2025-11-28 16:35:59 +0530

    do left join on messsages to get all the tables

--- models/intermediate/contacts_messages.sql
+++ models/intermediate/contacts_messages.sql
@@ -1,16 +1,14 @@
 select
-
     contacts.urn,
     contacts.full_name,
     contacts.designation,
+    contacts.inserted_at as contact_inserted_at,
     messages.content,
     messages.direction,
     messages.journey_name,
     messages.author_type,
-    contacts.inserted_at as contact_inserted_at,
-    messages.inserted_at as message_inserted_at,
-    messages.id as message_id
-
-
- from {{ ref('contacts_flatten') }} as contacts
-left join {{ ref('messages_flatten') }} as messages on contacts.whatsapp_id = messages.author_id
+    messages.id as message_id,
+    messages.inserted_at as message_inserted_at
+from {{ ref('messages_flatten') }} as messages 
+left join {{ ref('contacts_flatten') }} as contacts 
+on contacts.whatsapp_id = messages.author_id
