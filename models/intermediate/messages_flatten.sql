2ee14fe938b0ea4c6bdf77b1cc534905159ae793
Abdul Razzaq <shadsaiddiqui@gmail.com>
2025-12-01 14:12:29 +0530

    contacts-messages added interactive_body_text

--- models/intermediate/messages_flatten.sql
+++ models/intermediate/messages_flatten.sql
@@ -9,5 +9,6 @@ select
     JSON_EXTRACT_SCALAR(author, '$.id') AS author_id,
     JSON_EXTRACT_SCALAR(author, '$.name') AS author_name,
     JSON_EXTRACT_SCALAR(author, '$.type') AS author_type,
+    JSON_EXTRACT_SCALAR(interactive, '$.body.text') AS interactive_body_text,
     inserted_at
 from {{ source('sef_whatsapp_bot', 'messages') }}
